14747ab8587aef05588ec21adba62824974963cc
Alex Loughlin <aloughlin711@gmail.com>
2020-02-01 15:05:41 +0000

    Start script to run the calculator

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+const calculator = document.querySelector('.calculator')
+const inputs = calculator.querySelector('.inputs')
+const results = document.querySelector('.display-result')
+const displayinputs = document.querySelector('.display-inputs')
+
+
+
+inputs.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+
+  }
+})
